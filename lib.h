0aa60cd77e5b03fa44148420e960d36a3699668a
bcsyzwqhz <111332030+bcsyzwqhz@users.noreply.github.com>
2023-08-18 22:12:59 +0800

    增加库

---
+++ lib.h
@@ -0,0 +1,10 @@
+#include<windows.h>
+#include<iostream>
+#include<string>
+using namespace std;
+void movexy(short x, short y)
+{
+    COORD position={x, y};
+    HANDLE hOut=GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hOut, position);
+}
