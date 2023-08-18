0fea77aaa3ba1b048f60fed33902b78f875802e4
bcsyzwqhz <makerLai@163.com>
2023-08-18 21:35:33 +0800

    项目文件

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+using namespace std;
+void movexy(short x, short y){
+    COORD position={x, y};
+    HANDLE hOut=GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hOut, position);
+}
+int main(){
+	HWND h;
+	h=FindWindow("ConsoleWindowClass",NULL);
+    movexy(37, 7);
+    cout<<"反电子教室系统"<<endl;
+    cout<<endl;
+    cout<<"                                     目前可以反击:"<<endl;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED);
+    cout<<"                      红蜘蛛多媒体网络教室 - 输入 k-rs即可反击"<<endl;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
+    cout<<"                              极域电子教室 - 输入 k-jy即可反击"<<endl;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+    cout<<"                          伽卡他卡电子教室 - 输入 k-yk即可反击"<<endl;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+    cout<<"                                  启动成功后，窗口会自动消失，"<<endl;
+    cout<<"                                        不要重复打开！"<<endl;
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 10);
+    cout<<"			  注意：本系统仅支持Windows 10及以下系统，"<<endl;
+	cout<<"				Windows 11窗口不会消失！" <<endl;
+	cout<<"                                      建议使用管理员模式启动"<<endl;
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+    cout<<"    input >>> ";
+    string s;
+    cin>>s;
+    if(s=="k-rs"){
+        system("cls");
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED);
+        cout<<"                                  红蜘蛛反击系统启动成功"<<endl;
+        Sleep(1000);
+        ShowWindow(h,0);
+        while(1)
+            system("taskkill /f /t /im REDAgent.exe");
+    }
+    if(s=="k-jy"){
+        system("cls");
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
+        cout<<"                                  极域反击系统启动成功"<<endl;
+        Sleep(1000);
+        ShowWindow(h,0);
+        while(1)
+        system("taskkill /f /t /im StudentMain.exe");
+        
+    }
+    if(s=="k-yk"){
+        system("cls");
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+        cout<<"                                  伽卡他卡反击系统启动成功"<<endl;
+        Sleep(1000);
+        ShowWindow(h,0);
+        while(1)
+            system("taskkill /f /t /im Student.exe");
+    } 
+    return 0;
+}
+
