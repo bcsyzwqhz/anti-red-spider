33a2116b29dea61e46d42eefc08bf5a8593eb530
bcsyzwqhz <makerlai@163.com>
2023-09-07 18:43:07 +0800

    模块化

--- lib.h
+++ lib.h
@@ -12,3 +12,33 @@ void setcolor(int num)
 {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),num);
  } 
+ 
+void k_rs(bool enable)
+{
+	if(enable) system("taskkill /f /t /im REDAgent.exe");
+} 
+
+void k_jy(bool enable)
+{
+	if(enable) system("taskkill /f /t /im StudentMain.exe");
+}
+
+void k_yk(bool enable)
+{
+	if(enable) system("taskkill /f /t /im Student.exe");
+}
+void k_rj(bool enable)
+{
+	if(enable)
+	{
+		system("taskkill /f /t /im RJAgent.exe");
+        system("taskkill /f /t /im RJService.exe");
+        system("taskkill /f /t /im RG-CloudManagerRemote.exe");
+        system("taskkill /f /t /im CMApp.exe");
+        system("taskkill /f /t /im CMService.exe");
+        system("taskkill /f /t /im CMLauncher.exe");
+        system("taskkill /f /t /im Launcher.exe");
+        system("taskkill /f /t /im RG-CloudManagerRemote_Setup.exe");
+	}
+}
+
