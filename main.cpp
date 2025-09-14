ce6598e9c0d6b4ec21ea3a3a7b0d0716ec0824d3
bcsyzwqhz <makerlai@163.com>
2025-09-14 17:03:49 +0800

    perf:修改文字&修改命令

--- main.cpp
+++ main.cpp
@@ -7,27 +7,26 @@ int main()
     movexy(37,7);
     cout<<"反电子教室系统"<<endl;
     cout<<endl;
-    cout<<"                                     目前可以反击:"<<endl;
+    cout<<"                                     目前可以反击："<<endl;
     setcolor(4);
-    cout<<"                      红蜘蛛多媒体网络教室 - 输入 k-rs即可反击"<<endl;
+    cout<<"                      红蜘蛛多媒体网络教室 - 输入 rs 即可反击。"<<endl;
 	setcolor(11);
-    cout<<"                              极域电子教室 - 输入 k-jy即可反击"<<endl;
+    cout<<"                              极域电子教室 - 输入 jy 即可反击。"<<endl;
 	setcolor(12);
-    cout<<"                          伽卡他卡电子教室 - 输入 k-yk即可反击"<<endl;
+    cout<<"                          伽卡他卡电子教室 - 输入 gk 即可反击。"<<endl;
 	setcolor(7);
-    cout<<"                                锐捷云教室 - 输入 k-rj即可反击"<<endl;
+    cout<<"                                锐捷云教室 - 输入 rj 即可反击。"<<endl;
 	setcolor(6);
     cout<<"                                  启动成功后，窗口会自动消失，"<<endl;
     cout<<"                                        不要重复打开！"<<endl;
 	setcolor(10);
-    cout<<"			  注意：本系统仅支持Windows 10及以下系统，"<<endl;
-	cout<<"				Windows 11窗口不会消失！" <<endl;
-	cout<<"                                      建议使用管理员模式启动"<<endl;
+	cout<<"				注：Windows 11窗口不会消失！" <<endl;
+	cout<<"                                      建议使用管理员模式启动。"<<endl;
 	setcolor(7);
     cout<<"    input >>> ";
     string s;
     cin>>s;
-    if(s=="k-rs")
+    if(s=="rs")
 	{
         system("cls");
 		setcolor(4);
@@ -36,7 +35,7 @@ int main()
         ShowWindow(h,0);
         k_rs();
     }
-    if(s=="k-jy")
+    if(s=="jy")
 	{
         system("cls");
 		setcolor(11);
@@ -46,7 +45,7 @@ int main()
         k_jy();
         
     }
-    if(s=="k-yk")
+    if(s=="gk")
 	{
         system("cls");
 		setcolor(12);
@@ -55,7 +54,7 @@ int main()
         ShowWindow(h,0);
         k_yk();
     } 
-    if(s=="k-rj")
+    if(s=="rj")
 	{
 		system("cls");
 		setcolor(12);
