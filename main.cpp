ea18f235e5bd8fc62fc3606e7a2eeb9415ddcade
bcsyzwqhz <makerlai@163.com>
2025-09-25 21:52:20 +0800

    fix:非debug环境下显示不正确

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@ int main()
 {
     HWND h;
 	h=FindWindow(("ConsoleWindowClass"),NULL);
+	movexy(37,7);
 	if(!EnableDebugPrivilege())
-#ifdef DEBUG
-		cout<<GetLastError()<<endl;
-#endif
-    movexy(37,7);
+    {
+		#ifdef DEBUG
+		    cout<<GetLastError()<<endl;
+        #endif
+	}
     cout<<"反电子教室系统"<<endl;
     cout<<endl;
     cout<<"                                     目前可以反击："<<endl;
